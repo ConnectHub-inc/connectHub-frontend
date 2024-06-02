bd332ced795be4a33ad66cec55dc9c062a1ef831
fujtak <fuj.eng23@icloud.com>
2024-06-02 15:22:29 +0900

    feat: move link

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 import PageIndex from './pages/PageIndex';
 import PageTest from './pages/test/PageTest';
@@ -13,6 +13,15 @@ root.render(
         <Route path="/" element={ <PageIndex/> } />
         <Route path="/test/" element={ <PageTest/> } />
       </Routes>
+      <h2>Link</h2>
+      <ul>
+        <li>
+          <Link to="/">Index</Link>
+        </li>
+        <li>
+          <Link to="/test/">Test</Link>
+        </li>
+      </ul>
     </BrowserRouter>
   </React.StrictMode>
 );
--- frontend/src/pages/PageIndex.jsx
+++ frontend/src/pages/PageIndex.jsx
@@ -1,14 +1,9 @@
-import {Link} from 'react-router-dom'
-
 import PageIndexH1 from "../components/pages/index/PageIndexH1"
 
 export default function PageIndex() {
   return (
     <>
       <PageIndexH1 />
-      {/* TODO Linkを固定にするには？layoutの構築 */}
-      <Link to="/">Index</Link>
-      <Link to="/test/">Test</Link>
     </>
   )
 }
